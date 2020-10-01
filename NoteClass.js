6b961d303ca02fbe9b48f1d362b3d4b3a3e8dd51
DezertRose <57520792+DezertRose@users.noreply.github.com>
2020-10-01 23:48:03 +0300

    Create NoteClass.js

---
+++ NoteClass.js
@@ -0,0 +1,9 @@
+class Note{
+    constructor( id, title, text, time, state){
+        this.id = id
+        this.title = title
+        this.time = time
+        this.text = text
+        this.state = state
+    }
+}
