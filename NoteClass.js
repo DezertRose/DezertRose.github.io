acffd6f40f4a2b92cde8b4f5d03d04523fb0b59f
DezertRose <57520792+DezertRose@users.noreply.github.com>
2020-10-02 16:16:03 +0300

    Update NoteClass.js

--- NoteClass.js
+++ NoteClass.js
@@ -7,3 +7,21 @@ class Note{
         this.state = state
     }
 }
+
+function NoHTML(tempText) {
+    let help="";
+    for (let i=0; i<tempText.length; i++){
+        if (tempText[i]=='<') {help+= '&lt;'}
+        else if (tempText[i]=='>') {help+= '&gt;'}
+        else if (tempText[i]=='"') {help+= '&quot;'}
+        else if (tempText[i]=='&') {help+= '&amp;'}
+        else {help+=tempText[i];}
+    }
+    return help;
+}
+
+function getData(){
+    let dat=new Date()
+    dat = dat.toString()
+    return  dat.substring(0, 24)
+}
